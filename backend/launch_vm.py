d49a11458a52cbfaf87a6a9d86dab3b8e149550b
Yuvraj-Sandhu <yuvrajsandhu2502@gmail.com>
2025-06-20 13:56:58 -0400

    Added complete backend code

--- backend/launch_vm.py
+++ backend/launch_vm.py
@@ -1,53 +1,52 @@
 import oci
 import json
-import os
 
-with open("config.json") as f:
-    cfg = json.load(f)
+def launch_instance():
+    with open("config.json") as f:
+        cfg = json.load(f)
 
-oci_config = oci.config.from_file()
+    oci_config = oci.config.from_file()
 
-compute = oci.core.ComputeClient(oci_config)
-network = oci.core.VirtualNetworkClient(oci_config)
+    compute = oci.core.ComputeClient(oci_config)
+    network = oci.core.VirtualNetworkClient(oci_config)
 
-with open(cfg["ssh_key_path"]) as f:
-    ssh_key = f.read()
+    with open(cfg["ssh_key_path"]) as f:
+        ssh_key = f.read()
 
-launch_details = oci.core.models.LaunchInstanceDetails(
-    compartment_id=cfg["compartment_id"],
-    display_name="session-instance",
-    availability_domain=cfg["availability_domain"],
-    shape=cfg["shape"],
-    metadata={
-        "ssh_authorized_keys":ssh_key
-    },
-    source_details=oci.core.models.InstanceSourceViaImageDetails(
-        source_type="image",
-        image_id=cfg["image_id"],
-        boot_volume_size_in_gbs=50
-    ),
-    create_vnic_details=oci.core.models.CreateVnicDetails(
-        assign_public_ip=True,
-        subnet_id=cfg["subnet_id"],
-        display_name="session-vnic",
-        skip_source_dest_check=False
+    launch_details = oci.core.models.LaunchInstanceDetails(
+        compartment_id=cfg["compartment_id"],
+        display_name="session-instance",
+        availability_domain=cfg["availability_domain"],
+        shape=cfg["shape"],
+        metadata={
+            "ssh_authorized_keys":ssh_key
+        },
+        source_details=oci.core.models.InstanceSourceViaImageDetails(
+            source_type="image",
+            image_id=cfg["image_id"],
+            boot_volume_size_in_gbs=50
+        ),
+        create_vnic_details=oci.core.models.CreateVnicDetails(
+            assign_public_ip=True,
+            subnet_id=cfg["subnet_id"],
+            display_name="session-vnic",
+            skip_source_dest_check=False
+        )
     )
-)
 
-print("Launcing Instance ...")
-response = compute.launch_instance(launch_details)
-instance=response.data
-print(f"Instance ID: {instance.id}")
+    response = compute.launch_instance(launch_details)
+    instance=response.data
 
-waiter = oci.wait_until(
-    compute,
-    compute.get_instance(instance.id),
-    'lifecycle_state',
-    'RUNNING',
-    max_interval_seconds=300
-)
+    waiter = oci.wait_until(
+        compute,
+        compute.get_instance(instance.id),
+        'lifecycle_state',
+        'RUNNING',
+        max_interval_seconds=300
+    )
 
-vnic_attachments = compute.list_vnic_attachments(compartment_id=cfg["compartment_id"], instance_id=instance.id)
-vnic_id = vnic_attachments.data[0].vnic_id
-vnic = network.get_vnic(vnic_id).data
-print(f"Public IP: {vnic.public_ip}")
+    vnic_attachments = compute.list_vnic_attachments(compartment_id=cfg["compartment_id"], instance_id=instance.id)
+    vnic_id = vnic_attachments.data[0].vnic_id
+    vnic = network.get_vnic(vnic_id).data
+    
+    return vnic.public_ip
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import launch_vm
+from uuid import uuid4
+
 
 app = FastAPI()
 
-@app.get("/ping")
-def ping():
-    return {"message":"pong"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.post("/session")
+def create_session():
+    try:
+        session_id = str(uuid4())
+        public_ip = launch_vm.launch_instance(session_id)
+        return{"session_id":session_id, "ip":public_ip, "url":f"http://{public_ip}:6080/vnc.html"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+@app.delete("/session/{session_id}")
+def terminate_session(session_id:str):
+    try:
+        launch_vm.terminate_instance(session_id)
+        return {"message": f"Session {session_id} terminated successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
